eeb0a59d9910d781cdf2246e23f60b5fa5e02298
reyadrinkon <reyad34@student.sust.edu>
2022-06-07 07:41:37 +0600

    ......

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bookRoutes = require('./routes/bookRoutes');
 const bankRoutes = require('./routes/bankRoutes');
-
+const {MongoClient} = require('mongodb');
 const Book=require('./models/book')
 const Bank=require('./models/bank')
-
 
 // express app
 const app = express();
 
 // connect to mongodb & listen for requests
 const dbURI = "mongodb+srv://rinkon:rinkon123@cluster0.vwseqcq.mongodb.net/book-shop?retryWrites=true&w=majority";
-
+const client = new MongoClient(dbURI);
+await client.connect();
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
@@ -24,6 +24,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
@@ -36,11 +37,12 @@ app.get('/', (req, res) => {
   res.redirect('/books');
 });
 app.post('/', (req, res) => {
+  console.log(req.body)
   const book= new Book({
-    title:'book2',
-    Author:'rinkon',
-    quantity_available: 50,
-    price:23
+    title:req.body.title,
+    Author:req.body.Author,
+    quantity_available:req.body.quantity_available,
+    price:req.body.price
 
   })
   book.save()
@@ -55,10 +57,12 @@ app.get('/createaccount', (req, res) => {
   res.redirect('/banks');
 });
 app.post('/createaccount', (req, res) => {
+  const x=Bank.find()
+  console.log(db.Bank)
   const bank= new Bank({
-    ac_holder:'rinkon',
-    ac_id:'123',
-    balance:50
+    ac_holder:req.body.ac_holder,
+    ac_id:req.body.ac_id,
+    balance:req.body.balance
 
   })
   bank.save()
